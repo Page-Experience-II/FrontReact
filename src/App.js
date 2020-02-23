e2e36c723585daf1bae92b8d17ec68181b109816
Abdeljalil NACEUR <contact@naceur-abdeljalil.com>
2020-02-23 18:13:45 +0100

    REDUX : - Added.

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./styles.css";
+
+import Login from "./page/Login";
+import Home from "./page/Home";
+import Content from "./page/Content";
+import Journals from "./page/Journals";
+import Notifications from "./page/Notifications";
+import Search from "./page/Search";
+import SignUp from "./page/SignUp";
+import TestRedux from './Test/Test.redux';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Router forceRefresh={false}>
+        <Switch>
+          <Route push exact path="/" component={Login} />
+          <Route push exact path="/test" component={TestRedux} />
+          <Route push exact path="/signup/" component={SignUp} />
+          <Route push exact path="/home/" component={Home} />
+          <Route push exact path="/content/:article" component={Content} />
+          <Route push exact path="/journals/" component={Journals} />
+          <Route push exact path="/notifications/" component={Notifications} />
+          <Route push exact path="/search/:data" component={Search} />
+          <Route push exact path="/search/" component={Search} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './index.css';
+import App from './index';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from "react-redux";
 import { routerMiddleware } from "react-router-redux";
@@ -12,11 +13,11 @@ import reducers from './store/configureStore';
 const createHistory = require("history").createBrowserHistory;
 const history = createHistory();
 const middleware = routerMiddleware(history);
-
+ 
 const store = createStore(reducers, composeWithDevTools(applyMiddleware(middleware, promiseMiddleware, thunk)));
-
+ 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, document.getElementById('root'));
+   <Provider store={store}>
+       <App />
+   </Provider>, document.getElementById('root'));
 serviceWorker.unregister();
