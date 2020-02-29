3fd5f7edd2f3d2356ca7bb0e6e3fc1f6c3aac250
Abdeljalil NACEUR <contact@naceur-abdeljalil.com>
2020-02-29 15:27:50 +0100

    CONFLICT : - Fixed/

--- src/index.js
+++ src/index.js
@@ -15,35 +15,8 @@ const middleware = routerMiddleware(history);
 
 const store = createStore(reducers, composeWithDevTools(applyMiddleware(middleware, promiseMiddleware, thunk)));
 
-import Login from "./page/Login";
-import Home from "./page/Home";
-import Content from "./page/Content";
-import Journals from "./page/Journals";
-import Notifications from "./page/Notifications";
-import Search from "./page/Search";
-import SignUp from "./page/SignUp";
-import ProfilePage from "./page/ProfilePage";
-
-function App() {
-  return (
-    <div className="App">
-      <Router forceRefresh={false}>
-        <Switch>
-          <Route push exact path="/" component={Login} />
-          <Route push exact path="/signup/" component={SignUp} />
-          <Route push exact path="/profile/:user" component={ProfilePage} />
-          <Route push exact path="/home/" component={Home} />
-          <Route push exact path="/content/:article" component={Content} />
-          <Route push exact path="/journals/" component={Journals} />
-          <Route push exact path="/notifications/" component={Notifications} />
-          <Route push exact path="/search/:data" component={Search} />
-          <Route push exact path="/search/" component={Search} />
-        </Switch>
-      </Router>
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
-
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
+serviceWorker.unregister();
